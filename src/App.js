83525de1fdbf5492a1d0092dd065883149088fa6
krit <krittamethmek@gmail.com>
2026-07-20 11:21:29 +0700

    class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Header,Content, Footer } from './func-components';
+import Calender from './class-components'
 
 function App() {
 
   return (
     <>
       <Header />
+      <p><center><Calender/></center></p>
       <Content />
       <Footer/>
     </>
