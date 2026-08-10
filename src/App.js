dd622dda7543b3c3923a5e64eb72658d60d647c4
krit <krittamethmek@gmail.com>
2026-08-10 09:56:13 +0700

    refsfunc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { EventData2 } from './event-data';
+import RefsFunc from './refs-func';
 
 function App() {
 
   return (
   
-    <EventData2/>
+    <RefsFunc/>
   )
 }
 
